ada9d2040e965c5c217d1fa431e081d7fb2471f8
Cristian <jbcristian78@gmail.com>
2023-12-19 00:07:57 -0300

    Adicionando partials

--- database/database.js
+++ database/database.js
@@ -3,6 +3,6 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('guiaPress', 'root', '23268265', {
     host: 'localhost',
     dialect: 'mysql',
-})
+});
 
 module.exports = connection;
