c625182e5266254785b9a1eb890d8c4acecf4664
Cristian <jbcristian78@gmail.com>
2023-12-18 22:48:04 -0300

    Adicionado view EJS

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+//View engine
+app.set('view engine', 'ejs');
+
+//Static
+app.use(express.static('public'));
+
+//Body parser
+app.use(bodyParser.urlencoded({extend: false}));
+app.use(bodyParser.json());
 
 
-app.get("/", (req, res) => {
-    res.send("Bem vindo ao meu site")
+app.get("/", (req, res) => { //Rota principal
+    res.render('index')
 
 });
 
