7a999448c73198765b9b32253c68e378be059883
Cristian <jbcristian78@gmail.com>
2023-12-27 23:07:07 -0300

    Implementando controle de categoria e adicionando no banco

--- categoria/controlerC.js
+++ categoria/controlerC.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const Categoria = require("./Categorias"); //importando o model
+const slugify = require("slugify"); //adicionando o slug (Abreviação)
 
 
 router.get("/admin/categorias/novo", (req, res) => {
     res.render("admin/categorias/noticias")
 });
 
-router.get("/categorias/salvar", (req, res) => {
-    var titulo = req.body.titulo;
+router.post("/categoria/salvar", (req, res) => { //criando rota para salvar no banco de dados
+   let titulo = req.body.titulo;
 
-    if (titulo != undefined) {
+    if (titulo != null) {
 
+        Categoria.create({ //criando categoria com base no model
+            titulo: titulo,
+            slug: slugify(titulo)
+        }).then(() => {
+            res.redirect("/")
+        });
+        
     } else {
-        res.redirect("admin/categorias/noticias")
+        res.redirect("/admin/categorias/novo")
     }
 
 });
